5a6b7b034abdbb051b16f9820b81cd995e4950f0
Sarita <126271555+SaritaPhD@users.noreply.github.com>
2024-11-19 13:00:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, render_template 
-import os
+import os 
 from flask_cors import CORS, cross_origin
 from cnnClassifier.utils.common import decodeImage
 from cnnClassifier.pipeline.prediction import PredictionPipeline
